60becfa9c827a698725252c60dea3150e7b71c36
Oleg Golenischev <oleg-golenischev63@yandex.ru>
2022-11-08 23:04:55 +0300

    Tasks from seminar

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -35,7 +35,6 @@ int CountUniqueElements(int[] array)
             count++;
     return count;
 }
-// { 1, 9, 9, 0, 2, 8, 0, 9 } -> {1, 9, 0, 2, 8}
 int[] GetUniquesElements(int[] array)
 {
     int[] uniques = new int[CountUniqueElements(array)];
@@ -50,11 +49,11 @@ int[] GetUniquesElements(int[] array)
 }
 void SelectionSort(int[] array)
 {
-    for(int i=0;i<array.Length;i++)
+    for (int i = 0; i < array.Length; i++)
     {
         int index = i;
-        for(int j=i;j<array.Length;j++)
-            if(array[j]<array[index])
+        for (int j = i; j < array.Length; j++)
+            if (array[j] < array[index])
                 index = j;
         int tmp = array[i];
         array[i] = array[index];
@@ -63,18 +62,18 @@ void SelectionSort(int[] array)
 }
 int[,] GetFrequencyDictionary(int[] array)
 {
-   int[,] frequencyDictionary = new int[CountUniqueElements(array), 2];
-   int[] uniques = GetUniquesElements(array);
-   SelectionSort(uniques);
-   for(int i=0;i<uniques.Length;i++)
+    int[,] frequencyDictionary = new int[CountUniqueElements(array), 2];
+    int[] uniques = GetUniquesElements(array);
+    SelectionSort(uniques);
+    for (int i = 0; i < uniques.Length; i++)
     {
-        frequencyDictionary[i,0] = uniques[i];
-        frequencyDictionary[i,1] = Count(array, uniques[i]);
+        frequencyDictionary[i, 0] = uniques[i];
+        frequencyDictionary[i, 1] = Count(array, uniques[i]);
     }
     return frequencyDictionary;
 }
 int[] array = { 1, 9, 9, 0, 2, 8, 0, 9 };
 int[,] frequencyDictionary = GetFrequencyDictionary(array);
-for(int i=0;i<frequencyDictionary.GetLength(0);i++)
-    Console.WriteLine($"Element {frequencyDictionary[i,0]} contains "+ 
-                        $"{frequencyDictionary[i,1]} times");
+for (int i = 0; i < frequencyDictionary.GetLength(0); i++)
+    Console.WriteLine($"Element {frequencyDictionary[i, 0]} contains " +
+                        $"{frequencyDictionary[i, 1]} times");
