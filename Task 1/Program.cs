0799d70f56866ddda9035e795f387fe13642cf08
Oleg Golenischev <oleg-golenischev63@yandex.ru>
2022-11-08 22:56:37 +0300

    Tasks from seminar

---
+++ Task 1/Program.cs
@@ -0,0 +1,40 @@
+﻿int[,] CreateMatrix(int rows, int columns, int minimum, int maximum)
+{
+    int[,] matrix = new int[rows, columns];
+    Random random = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = random.Next(minimum, maximum);
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} ");
+        Console.WriteLine();
+    }
+}
+int ReadIntegerNumber(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+int SumOfMainDiagonal(int[,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        sum += matrix[i, i];
+    return sum;
+}
+int length = ReadIntegerNumber("Enter amount of rows: ");
+int minValue = ReadIntegerNumber("Enter start of number range: ");
+int maxValue = ReadIntegerNumber("Enter end of number range: ");
+int[,] matrix = CreateMatrix(length, length, minValue, maxValue);
+Console.WriteLine("Your matrix: ");
+PrintMatrix(matrix);
+if (matrix.GetLength(0) == matrix.GetLength(1))
+    Console.WriteLine($"Sum of main diagonal: {SumOfMainDiagonal(matrix)}");
+else
+    Console.WriteLine("Matrix is not square");
---
+++ Task 2/Program.cs
@@ -0,0 +1,80 @@
+﻿int[] CreateArray(int length, int minimum, int maximum)
+{
+    int[] array = new int[length];
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+        array[i] = random.Next(minimum, maximum);
+    return array;
+}
+void PrintArray(int[] array)
+{
+    foreach (int element in array)
+        Console.Write($"{element} ");
+    Console.WriteLine();
+}
+int Count(int[] array, int value)
+{
+    int count = 0;
+    foreach (int element in array)
+        if (element == value)
+            count++;
+    return count;
+}
+bool Contains(int[] array, int value, int index)
+{
+    for (int i = 0; i < index; i++)
+        if (array[i] == value)
+            return true;
+    return false;
+}
+int CountUniqueElements(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (Contains(array, array[i], i) == false)
+            count++;
+    return count;
+}
+// { 1, 9, 9, 0, 2, 8, 0, 9 } -> {1, 9, 0, 2, 8}
+int[] GetUniquesElements(int[] array)
+{
+    int[] uniques = new int[CountUniqueElements(array)];
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (Contains(array, array[i], i) == false)
+        {
+            uniques[index] = array[i];
+            index++;
+        }
+    return uniques;
+}
+void SelectionSort(int[] array)
+{
+    for(int i=0;i<array.Length;i++)
+    {
+        int index = i;
+        for(int j=i;j<array.Length;j++)
+            if(array[j]<array[index])
+                index = j;
+        int tmp = array[i];
+        array[i] = array[index];
+        array[index] = tmp;
+    }
+}
+int[,] GetFrequencyDictionary(int[] array)
+{
+   int[,] frequencyDictionary = new int[CountUniqueElements(array), 2];
+   int[] uniques = GetUniquesElements(array);
+   SelectionSort(uniques);
+   for(int i=0;i<uniques.Length;i++)
+    {
+        frequencyDictionary[i,0] = uniques[i];
+        frequencyDictionary[i,1] = Count(array, uniques[i]);
+    }
+    return frequencyDictionary;
+}
+int[] array = { 1, 9, 9, 0, 2, 8, 0, 9 };
+int[,] frequencyDictionary = GetFrequencyDictionary(array);
+for(int i=0;i<frequencyDictionary.GetLength(0);i++)
+    Console.WriteLine($"Element {frequencyDictionary[i,0]} contains "+ 
+                        $"{frequencyDictionary[i,1]} times");
